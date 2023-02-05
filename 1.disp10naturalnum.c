fe03e17f5531c5c325722799083b3a7db059af05
Abhishek kumar <124049496+abhiiraj118@users.noreply.github.com>
2023-02-05 11:21:19 +0530

    Add files via upload

---
+++ 1.disp10naturalnum.c
@@ -0,0 +1,13 @@
+//1.write a program in c to display the first 10 natural numbers.
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("The first 10 natural numbers are:\n");
+    for (i=1;i<=10;i++)
+    {
+        printf("%d ",i);
+    }
+printf("\n");
+}
