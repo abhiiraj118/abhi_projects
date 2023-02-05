6622d9308908c3f1ec813433f25172967b6c00c7
Abhishek kumar <124049496+abhiiraj118@users.noreply.github.com>
2023-02-05 11:16:19 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-// 6. write a program in c to display the pattern like a right angle triangle using an asterisk.
+// 2. write a c program to find the sum of first 10 nqtural numbers.
+
+
+
 
 #include <stdio.h>
 int main()
 {
-   int i,j,rows;
-   printf("Input number of rows : ");
-   scanf("%d",&rows);
-   for(i=1;i<=rows;i++)
-   {
-    for(j=1;j<=i;j++)
-       printf("*");
-    printf("\n");
-   }
+    int  i, sum = 0;
+
+    printf("The first 10 natural numbers is :\n");
+ 
+    for (i = 1; i <= 10; i++)
+    {
+        sum = sum + i;
+        printf("%d ",i);
+    }
+    printf("\nThe Sum is : %d\n", sum);
 }
